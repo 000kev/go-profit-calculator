e1671a818810cef99dc6bbd6df789c4d5a3c8335
Kevin <Kevin.Kumasamba@anxend.com>
2024-11-29 20:14:10 +0200

    A basic profit calculator using EBT, profit and ratio calculations

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Print
+	var revenue, expenses, tax float64
+	fmt.Print("Enter your revenue amount in ZAR: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Enter your expense amount in ZAR: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Enter your tax rate: ")
+	fmt.Scan(&tax)
+
+	EBT := revenue - expenses
+	profit := revenue - ( revenue * (tax/100))
+	ratio := EBT / profit
+
+	fmt.Print("Your Earnings Before Tax (EBT) is ")
+	fmt.Println(EBT, "ZAR")
+	fmt.Print("Your Profit is ")
+	fmt.Println(profit, "ZAR")
+	fmt.Print("Your Ratio is ")
+	fmt.Println(ratio)
 }
